684b81fdfd38f8868f2362fb484ea7d305a38790
Sergej Kozin <sergej.kozin@vdombox.ru>
2018-02-06 11:59:59 +0700

    Fix order of loading of js files

--- GmailEAC/google-chrome/main.js
+++ GmailEAC/google-chrome/main.js
@@ -1,4 +1,5 @@
-function inject_script(script, remove) {
+
+function inject_script(script, remove, callback) {
     if (remove == null) remove = true;
 
     var injection = document.createElement('script');
@@ -12,13 +13,28 @@ function inject_script(script, remove) {
     else 
     	return;
 
-    if( remove ) {
-        injection.onload = function() {
+    injection.onload = function() {
+        if (remove)
             this.parentNode.removeChild(this);
-        }
-    }
+
+        if (callback)
+            callback();
+    };
 
     (document.head || document.documentElement).appendChild(injection);
+}
+
+
+
+
+function inject_all_files(files) {
+    function next() {
+        if (!files.length) return;
+        var file = files.shift();
+        inject_script({fileName: file}, true, next);
+    }
+
+    next();
 }
 
 
@@ -32,38 +48,43 @@ function main() {
         window.Extension.EXTENSION_URL = '${chrome.runtime.getURL('')}';
     `;
 
+    inject_script({code: send_variables});
 
-    inject_script({code : send_variables});
 
-    inject_script({fileName : 'js/utils.js'});
+    var requirements = [
+        'js/utils.js',
+        'js/maindb.js',
+        'js/common.js',
+        'js/ajax.js',
 
-    inject_script({fileName : 'js/maindb.js'})
-
-    inject_script({fileName : 'js/common.js'});
-
-    inject_script({fileName : 'js/ajax.js'});
+        'js/test.js'
+    ];
 
 
     var currentHost = window.location.host.toLowerCase();
 
     if (currentHost === 'mail.google.com') {
-        inject_script({fileName : 'js/gmail_api.js'});
-
-        inject_script({fileName : 'js/eac.js'});
-
-        inject_script({fileName : 'js/eac_processing.js'});
-
-        inject_script({fileName : 'js/gmail.js'});
+        requirements = requirements.concat([
+            'js/gmail_api.js',
+            'js/eac.js',
+            'js/eac_processing.js',
+            'js/gmail.js'
+        ]);
     }
 
 
     if (['outlook.live.com', 'outlook.office.com', 'outlook.office365.com'].indexOf(currentHost) > -1) {
-        inject_script({fileName : 'js/outlook.js'});
+        requirements = requirements.concat([
+            'js/outlook.js'
+        ]);
     }
 
 
-    document.__EAC_Extension_Injected = true;
+    inject_all_files(requirements);
+
     console.log('EAC Plugin started');
+
+    document.__EAC_Extension_Injected = true;
 }
 
 
